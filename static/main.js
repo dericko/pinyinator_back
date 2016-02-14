38f780ea821e225cc0d8eecc91c16aa76b5bbf11
Kyle Wilcox <wilcox32ahs@gmail.com>
2016-02-14 14:52:52 -0500

    Semi format working

--- static/main.js
+++ static/main.js
@@ -5,8 +5,8 @@ $(function() {
     var text = $('#chinese-text').val();
     var url = "pinyinify";
     var data = {data:text};
-    console.log(text);
-    console.log(data);
+    // console.log(text);
+    // console.log(data);
     $.ajax({
       type: "POST",
       url: url,
@@ -18,13 +18,36 @@ $(function() {
 });
 
 var displayPairs = function (data) {
-  console.log(data);
-  var pairs = data.sentence;
-  var $display = $('p');
-  var stringRep = '';
-  pairs.forEach(function (pair) {
-    console.log(pair[0] + ': ' + pair[1]);
-    stringRep = stringRep + ' ' + pair[0];
+  var $displayArea = $('#chinese-output');
+  console.log($displayArea);
+  var titlePairs = data.title;
+  var paragraphs = data.paragraphs;
+  var $paragraphWrap = $('<div></div>');
+  var $titleChar = $('<h1></h1>');
+  var $titlePiyin = $('<h2></h2>');
+  // var titleChar = '';
+  // var pinyinChar = '';
+  titlePairs.forEach(function (pair) {
+    var stringRep =  pair[0] + pair[1]
+    $titleChar.append(pair[[0]]);
+    $titlePiyin.append(pair[1]);
   });
-  console.log(stringRep);
+
+
+  paragraphs.forEach(function (paragraph) {
+    var $displayChar = $('<p></p>');
+    var $displayPinyin = $('<p></p>');
+
+    paragraph.forEach(function (pair) {
+      var stringRep =  pair[0] + pair[1];
+      $displayChar.append(pair[0]);
+      $displayPinyin.append(pair[[1]]);
+    });
+    $paragraphWrap.append($displayPinyin);
+    $paragraphWrap.append($displayChar);
+  });
+
+  $displayArea.append($titlePiyin);
+  $displayArea.append($titleChar);
+  $displayArea.append($paragraphWrap);
 }
